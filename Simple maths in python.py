87bb256ff64c96067c6ba0b92d7ba07c64d970c3
abhiraaj214 <44207395+abhiraaj214@users.noreply.github.com>
2018-10-16 22:41:35 +0530

    Add files via upload

---
+++ Simple maths in python.py
@@ -0,0 +1,24 @@
+# Static:
+x = 5
+y = 3
+
+# Dynamic:
+# Asking the user for 2 numbers.
+x = int(input("\nEnter first number: "))
+y = int(input("Enter second number: "))
+
+# Addition
+a = x + y
+print("\nAddition of two numbers is:", a, "\n")
+
+# Subtraction
+s = x - y
+print("Subtraction of two numbers is:", s, "\n")
+
+# Multiplication
+m = x * y
+print("Multiplication of two numbers is:", m, "\n")
+
+# Division
+m = x / y
+print("Division of two numbers is:", m, "\n")
